78309f116d0b34e0d44cd0eddeed9ccf560255a4
cheetahx7x <jony.fabing@gmail.com>
2020-11-25 14:04:40 +0100

    Fixed the mod

--- control.lua
+++ control.lua
@@ -7,13 +7,7 @@ end
 function scan(position,surface)
     local p = position
     local r = settings.global["initial-scan-radius"].value+game.forces["player"].get_item_launched("scanner-satellite")*settings.global["consecutive-scan-radius-expansion"].value
-    for chunk in surface.get_chunks() do
-        if(chunk.x >= p.x-r and chunk.x <= p.x+r and chunk.y >= p.y-r and chunk.y <= p.y+r) then
-            surface.request_to_generate_chunks({chunk.x,chunk.y},0)
-            surface.force_generate_chunk_requests()
-            game.forces["player"].chart(surface,chunk.area)
-        end
-    end
+    game.forces["player"].chart(surface,{{x=p.x+r*32,y=p.y+r*32},{x=p.x-r*32,y=p.y-r*32}})
 end
 
 script.on_event(defines.events.on_rocket_launched,Rocket_launched)
